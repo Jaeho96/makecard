6989eba634caa496d0ae15915a5ded184de65248
JaehoY <qnddjek7@naver.com>
2021-03-10 13:30:29 +0900

    Home화면

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import css from "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nav-Start */}
+      <div className="Nav">
+        <img src="" alt="로고" className="nav-logo"></img>
+        <div className="nav-item">
+          <div>Home</div>
+          <div>카테고리1</div>
+          <div>카테고리2</div>
+        </div>
+      </div>
+      {/* Nav-End */}
+
+      {/* Main-Start */}
+      <div className="Main">
+        <div className="main-text">
+          <p>지금 바로 </p>
+          <p>당신만의 명함을 만들어보세요!</p>
+        </div>
+        <div className="main-btn"> Get Start</div>
+      </div>
+      {/* Main-End */}
     </div>
   );
 }

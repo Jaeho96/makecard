f6056f6852994b14b1f2e948906789599c05b829
JaehoY <qnddjek7@naver.com>
2021-03-11 15:00:04 +0900

    API 추가해야함

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../logo192.png";
 
 function Signin() {
-  return <div>로그인화면</div>;
+  return (
+    <div className="Main-sign">
+      <div className="sign-logo">
+        <img src={Logo} alt="로고" width="246px" height="162px" />
+      </div>
+      <div className="sign-text">
+        <p> 지금 회원가입하시면 </p>
+        <p> 명함이 5초만에 뚝딱!</p>
+      </div>
+      <Link to="/signin" className="sign-btn">
+        카카오톡으로 회원가입
+      </Link>
+      <Link to="/signin" className="sign-btn2">
+        이미 가입하셨다면, 바로 로그인하기
+      </Link>
+    </div>
+  );
 }
 
 export default Signin;
